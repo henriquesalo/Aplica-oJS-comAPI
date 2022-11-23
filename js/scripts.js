9be8be202333fd47b5ab6540da4765143db1b37d
henriquesalo <henriquersalomao@gmail.com>
2022-11-22 23:06:35 -0300

    att js

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,10 @@ const umidityElement = document.querySelector("#umidity span");
 const windElement = document.querySelector("#wind span");
 
 //Funções
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+}
+
 const showWeatherData = (city) =>{
     console.log(city);
 }
