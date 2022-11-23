1ed7a20b873783fcd831eb33eba2905e7259d4db
henriquesalo <henriquersalomao@gmail.com>
2022-11-23 19:56:27 -0300

    att v.1

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,10 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const humidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
 
 //Funções
 const getWeatherData = async(city) => {
@@ -29,10 +31,15 @@ const showWeatherData = async (city) =>{
    cityElement.innerText = data.name;
    tempElement.innerText = parseInt(data.main.temp);
    descElement.innerText = data.weather[0].description;
-   weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+   weatherIconElement.setAttribute(
+    "src", 
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
    humidityElement.innerText = `${data.main.humidity}%`;
    windElement.innerText = `${data.wind.speed}km/h`;
+
+   weatherContainer.classList.remove("hide");
 };
 
 //Eventos
@@ -41,5 +48,13 @@ searchBtn.addEventListener("click", (e) =>{
 
     const city = cityInput.value;
 
-    console.log(city);
+    showWeatherData(city);
+});
+//Funcionar ao Apertar ENTER
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code === "Enter" ){
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
 });
