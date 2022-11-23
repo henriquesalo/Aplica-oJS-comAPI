75821cf6bd9590e3f32953a8c1052f4a683eb682
henriquesalo <henriquersalomao@gmail.com>
2022-11-22 22:13:32 -0300

    att js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-// API Key = 161b6a38d422348d75fb526a70d0f1eb
+//Variáveis e Seleção dos elementos
+const apiKey = "161b6a38d422348d75fb526a70d0f1eb";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+//Funções
+const showWeatherData = (city) =>{
+    console.log(city);
+}
+
+//Eventos
+searchBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    console.log(city);
+});
